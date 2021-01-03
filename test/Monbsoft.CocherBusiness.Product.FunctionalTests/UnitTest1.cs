bc1e1f873f53df9a5f41c4ab8fcadac5c093cdd5
oliver254 <oliver254@hotmail.fr>
2021-01-03 02:55:58 +0100

    #1 : Add functional tests

---
+++ test/Monbsoft.CocherBusiness.Product.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Monbsoft.CocherBusiness.Product.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
